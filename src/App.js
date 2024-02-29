aa3286c915a6b3673d2a64cbf749d4a93962add3
Shaurya <shaurya@Shauryas-MacBook-Air.local>
2024-02-29 22:13:45 +0530

    certificate-section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/Home/Home";
 import Navbar from "./component/Navbar/Navbar";
 import About from "./pages/About/About";
 import Project from "./pages/Project/Project";
+import Certificates from "./pages/Certificates/Certificates";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from "react";
@@ -21,13 +22,13 @@ function App() {
         <About />
       </section>
 
-      <section class="full-screen">
+      <section id='projects' class="full-screen">
         <Project />
       </section>
 
-      {/* <section class="full-screen">
-        <Certificate />
-      </section>  */}
+      <section id='certificate' class="full-screen">
+        <Certificates />
+      </section> 
     </div>
   );
 }
---
+++ src/pages/Certificates/certificate.js
@@ -0,0 +1,23 @@
+const certificateList = [
+    {
+        imgUrl: 'https://static.vecteezy.com/system/resources/previews/006/692/271/non_2x/document-icon-template-black-color-editable-document-icon-symbol-flat-illustration-for-graphic-and-web-design-free-vector.jpg',
+        driveUrl: '',
+        title: 'Low Level Design'
+    },
+    {
+        imgUrl: 'https://static.vecteezy.com/system/resources/previews/006/692/271/non_2x/document-icon-template-black-color-editable-document-icon-symbol-flat-illustration-for-graphic-and-web-design-free-vector.jpg',
+        driveUrl: '',
+        title: 'Spring/SpringBoot'
+    },
+    {
+        imgUrl: 'https://static.vecteezy.com/system/resources/previews/006/692/271/non_2x/document-icon-template-black-color-editable-document-icon-symbol-flat-illustration-for-graphic-and-web-design-free-vector.jpg',
+        driveUrl: '',
+        title: 'DSA'
+    },
+    ,{
+        imgUrl: 'https://static.vecteezy.com/system/resources/previews/006/692/271/non_2x/document-icon-template-black-color-editable-document-icon-symbol-flat-illustration-for-graphic-and-web-design-free-vector.jpg',
+        driveUrl: '',
+        title: 'JAVA'
+    }
+]
+export default certificateList;
