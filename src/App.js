15e6501bea47e0947d024d444bd1fc274699593d
Shaurya <shaurya@Shauryas-MacBook-Air.local>
2024-03-04 22:37:10 +0530

    responsive

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Navbar from "./component/Navbar/Navbar";
 import About from "./pages/About/About";
@@ -14,8 +14,9 @@ function App() {
 
   return (
     <div className="App">
+      <Navbar />
       <section id="home" className="full-screen" >
-        <Navbar />
+      
         <Home />
       </section>
 
